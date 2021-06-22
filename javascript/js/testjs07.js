848204047f831540485278d7b93cc1d200727108
yvandouenel <y.douenel@coopernet.fr>
2021-06-22 15:29:02 +0200

    Retour sur les objets et les structures de contrôle.

--- javascript/js/testjs07.js
+++ javascript/js/testjs07.js
@@ -1,30 +1,29 @@
-// Tableau à index
-const fruits = ["Banane", "Cerise", "Pomme"];
+class Personne {
+  constructor(name, lastname) {
+    this.firstname = name;
+    this.lastname = lastname;
+  }
+  introduceMySelf() {
+    console.log(`Bonjour, je m'appelle ${this.firstname}`);
+  }
+}
+// Ici p1 a accès au prototype de Personne et d'Object
+const p1 = new Personne("Bob", "Dylan");
+console.log(`p1 : `, p1);
 
-/* console.log("fruits", fruits);
-console.log("Taille du tableau fruits", fruits.length); */
-
-// Parcours du tableau fruits
-fruits.forEach(function (fruit, index, tab) {
-  /*  console.log(`fruit : `, fruit);
-  console.log(`index : `, index);
-  console.log(`tableau : `, tab); */
-});
-let new_length = 0;
-// Ajout d'un élément en fin de tableau
-fruits.push("Fraise", "Poire", "Kiwi");
-console.log(`Fruits : `, fruits);
-
-// Suppression ou remplacement à l'intérieur du tableau
-// Le premier argument indique le début du changement du tableau
-// Le deuxième argument indique combien d'éléments sont supprimés
-// Le troisième argument indique les éléments ajoutés
-// La méthode renvoie le tableau des éléments supprimés
-let remove_fruits = fruits.splice(3, 3, "Papaye", "Orange", "Mangue");
-console.log(`Fruits : `, fruits);
-console.log(`Fruits supprimés : `, remove_fruits);
-
-// Transformation d'un tableau via ma méthode map
-const fruits_li = fruits.map((fruit) => `<li>${fruit}</li>`);
-
-console.log(`fruits après le map : `, fruits_li);
+// Ajout au prototype d'Object
+Object.prototype.introduceMySelf = function () {
+  console.log(`Hello, my name is ${this.firstname}`);
+};
+Object.prototype.pikachu = "Jean-Michel";
+p1.introduceMySelf();
+// Objet Littéral ou syntaxe JSON
+// Javascript Object Notation
+// p2 n'a accès qu'au prototype d'Object
+const p2 = {
+  firstname: "Bob",
+  lastname: "Dylan",
+};
+console.log(`p2 : `, p2);
+p2.introduceMySelf();
+console.log(`pikachu : `, p2.pikachu);
---
+++ javascript/js/testjs08.js
@@ -0,0 +1,22 @@
+const p1 = {
+  firstname: "Bob",
+  lastname: "Marley",
+  vote: function () {
+    console.log(`${this.firstname} a voté`);
+  },
+};
+for (property in p1) {
+  console.log(`property : `, property);
+  console.log(`property value : `, p1[property]);
+}
+/* // Boucle for avec incrémentation (augmentation de 1)
+for (let i = 0; i < 10; i++) {
+  console.log(`i : `, i);
+}
+console.log(`i : `, i); */
+
+console.log(`Prénom de p1 : `, p1["firstname"]);
+console.log(`Prénom de p1 : `, p1.firstname);
+
+p1.vote();
+p1["vote"]();
