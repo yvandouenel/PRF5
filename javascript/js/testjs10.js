70b8bbbb2010e8b96317177b6b87f8fa162c7bb6
yvandouenel <y.douenel@coopernet.fr>
2021-06-23 15:29:39 +0200

    fin des tests sur les événements

--- javascript/js/testjs10.js
+++ javascript/js/testjs10.js
@@ -5,7 +5,26 @@
 const btn1 = document.getElementById("btn1");
 
 // Affectation d'une fonction gestionnaire d'événement à la propriété onclick
-btn1.onclick = function () {
+
+// Avec le mot clé function, "this" est "relié" (bind) à l'instance
+// de l'objet appelant (ici btn1)
+// Avec les fonctions fléchées, this dépend du contexte dans lequel
+// la fonction a été déclarée. Ici, le contexte étant global, this
+// est un alias de window
+
+btn1.onclick = () => {
   // ! signifie "inverse", le texte va se cacher ou s'afficher
   document.querySelector("p").hidden = !document.querySelector("p").hidden;
+
+  console.log(`this : `, this);
+};
+
+const link = document.querySelector("a");
+link.onclick = function (event) {
+  // Supprime l'action par défaut de l'événement en cours
+  // Très utile pour les liens et les formulaires
+  event.preventDefault();
+
+  console.log(`source la plus profonde de l'événement : `, event.target);
+  console.log(`tu as cliqué sur le lien`);
 };
