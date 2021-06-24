e04be1c9953d65cc2cab626a53b1d062aa2f0386
yvandouenel <y.douenel@coopernet.fr>
2021-06-24 11:15:43 +0200

    fonction createMarkup commentée.

--- javascript/exos/exoDOMEvent.js
+++ javascript/exos/exoDOMEvent.js
@@ -1,21 +1,31 @@
+/**
+ * Fonction qui crée une instance d'élément du DOM
+ * @param {string} markup_name
+ * @param {string} text
+ * @param {Objet} parent DOM element
+ * @param {Objet} attribute
+ * @return {0bject} element du DOM
+ */
 function createMarkup(markup_name, text, parent, attribute) {
+  // la variable markup stocke le retour de la fonction
+  // createElement. Ce retour a pour type "object". Cet objet est
+  // un élément du DOM. markup pointe donc (ou stocke) vers  un élément du dom
   const markup = document.createElement(markup_name);
+
+  // Ajout de texte à l'élément du dom markup
   markup.textContent = text;
+
+  // Positionnement de markup en tant que dernier fils de l'élément
+  // du dom "parent"
   parent.appendChild(markup);
+
+  // Si attribute est défini et qu'il a bien la propriété name,
+  // alors on ajoute à markup l'attribut qui a pour nom
+  // attribute.nom et pour valeur attribute.value
   if (attribute && attribute.hasOwnProperty("name")) {
     markup.setAttribute(attribute.name, attribute.value);
   }
+
+  // La fonction renvoie l'élément du DOM markup
   return markup;
 }
-
-// création du bouton
-const btn = createMarkup("button", "Ajouter un paragraphe", document.body);
-
-// Affectation d'un gestionnaire d'événement onclick sur le bouton
-btn.onclick = function () {
-  createMarkup(
-    "p",
-    " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur minima doloremque voluptate voluptatum odit mollitia culpa in consectetur magnam reprehenderit eos accusamus ex adipisci, molestias architecto voluptates. Quis, sequi nostrum. ",
-    document.body
-  );
-};
